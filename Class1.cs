a2c9a41788760525a1e1795415245277f28e41b2
zbcnaba <zbcnaba@ZBC-E-AH-502BP7>
2021-08-31 08:36:29 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Square
+    {
+        private int SquareLength() 
+        {
+            return 5;
+        }
+    }
+}
